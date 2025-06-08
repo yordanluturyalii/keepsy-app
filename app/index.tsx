d74d85d5c1cababaf716fe2a2322f93bd10c3e8b
yordanluturyalii <yordanluturyali@gmail.com>
2025-06-08 19:23:06 +0700

    :art: refactor: lint & format code

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,12 @@
-import { Link } from "expo-router"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
-    return (
-        <SafeAreaView className="gap-y-3">
-            <Link href={'/'}>Halow</Link>
-        </SafeAreaView>
-    )
-}
+	return (
+		<SafeAreaView className="gap-y-3">
+			<Link href={"/"}>Halow</Link>
+		</SafeAreaView>
+	);
+};
 
-export default HomeScreen
+export default HomeScreen;
